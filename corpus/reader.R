39592164d3aa5467f08d48ed54f79207433a49cf
buechel <sven-buechel@gmx.de>
2019-02-09 15:37:33 +0100

    Cleaning up

--- corpus/reader.R
+++
@@ -1,5 +0,0 @@
-rm(list=ls())
-source("combine.R")
-df.reader = corpus.final[c("sentence", "Val.R", "Aro.R","Dom.R")]
-colnames(df.reader) = c("Sentence", "Valence","Arousal","Dominance")
-rm(corpus.final)
--- corpus/writer.R
+++
@@ -1,5 +0,0 @@
-rm(list=ls())
-source("combine.R")
-df.writer = corpus.final[c("sentence", "Val.W", "Aro.W","Dom.W")]
-colnames(df.writer) = c("Sentence", "Valence","Arousal","Dominance")
-rm(corpus.final)
